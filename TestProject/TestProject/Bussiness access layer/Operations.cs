e678bc42e89099904959e46ed317d9fb7dc1ce66
Your Name <you@example.com>
2019-12-19 17:43:20 +0530

    Addition method added

---
+++ TestProject/TestProject/Bussiness access layer/Operations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestProject.Bussiness_access_layer
+{
+    public class Operations
+    {
+        public int Addition()
+        {
+            int a=10;
+            int b=20;
+            int c = a + b;
+            return c;
+
+        }
+    }
+}
